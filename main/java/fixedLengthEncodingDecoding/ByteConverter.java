5312c256c48cd9f1179418488969bad5d210e817
Ankitagole <ankita17@navgurukul.org>
2019-08-14 15:19:51 +0530

    storing value in array

--- main/java/fixedLengthEncodingDecoding/ByteConverter.java
+++ main/java/fixedLengthEncodingDecoding/ByteConverter.java
@@ -1,6 +1,7 @@
 package fixedLengthEncodingDecoding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ByteConverter {
@@ -13,15 +14,24 @@ public class ByteConverter {
             }else {
                 temp.append(0);
             }
-            if(temp.length()==10){
+            if(temp.length()==8){
                 bytes.add((byte) Integer.parseInt(temp.substring(2), 2));
                 temp = new StringBuilder();
             }
         }
 
         bytes.add((byte) Integer.parseInt(temp.toString(), 2));
-        System.out.println(bytes);
-        return new byte[bytes.size()];
+        System.out.println(Arrays.toString(getBinaryArray(bytes)));
+        return getBinaryArray(bytes);
 
+    }
+    private byte[] getBinaryArray(List<Byte> bytes){
+        int i = 0;
+        byte[] bytesArray = new byte[bytes.size()];
+        for (Byte bits :bytes) {
+             bytesArray[i] = bits;
+             i = i +1;
+        }
+        return bytesArray;
     }
 }
